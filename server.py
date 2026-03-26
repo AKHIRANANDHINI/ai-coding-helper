22d66702038cf570eb1270d81ca819fe293ce3fd
2303A51516 <2303A51516@sru.edu.in>
2026-03-26 17:38:52 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -18,6 +18,11 @@ def explain_code():
 
     return jsonify({"reply": reply})
 
+import os
+
 if __name__ == "__main__":
     print("Server is starting...")
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
